686445a42cc279cee00f46aa699417dedb4670e5
Haoming Chen <chenhaomingsz@gmail.com>
2017-02-22 22:22:56 -0500

    add reference paper

--- HW2-Feature Descriptors, Homographies & RANSAC/code/briefRotTest.m
+++ HW2-Feature Descriptors, Homographies & RANSAC/code/briefRotTest.m
@@ -3,6 +3,14 @@ function [] = briefRotTest()
 % load images and rotate, compute feature mathces
 
 im = imread('../data/model_chickenbroth.jpg');
+im = im2double(im);
+% turn the images into gray scale
+if size(im, 3) == 3
+    im = rgb2gray(im);
+end
+
+% compute descriptors 
+[~, desc1] = briefLite(im);
 
 % initialize matrix to store number of matches
 num_matches = zeros(1, 35);
@@ -13,20 +21,14 @@ for angle = 0:10:350
     
     % rotate the image by certain angle
     im_rotate = imrotate(im, angle);
-    
-    im = im2double(im);
-    im_rotate = im2double(im_rotate);
-    
+       
     % turn the images into gray scale
-    if size(im, 3) == 3
-        im = rgb2gray(im);
-    end
+    im_rotate = im2double(im_rotate);
     if size(im_rotate, 3) == 3
         im_rotate = rgb2gray(im_rotate);
     end
 
-    % compute descriptors for both images
-    [~, desc1] = briefLite(im);
+    % compute descriptors 
     [~, desc2] = briefLite(im_rotate);
 
     % compute feature matches of two images
@@ -36,6 +38,11 @@ for angle = 0:10:350
     
 end 
 
+% num_matches = rand(1, 36);
+
 % construct bar graph (rotation angle VS num_matches)
-bar(0:10:350, num_matches, 'c', 'LineWidth', 0.75);
+bar(0:10:350, num_matches, 'g', 'width', 1);
+xlabel('Degree of rotation');
+ylabel('Number of correct matches');
+
 
