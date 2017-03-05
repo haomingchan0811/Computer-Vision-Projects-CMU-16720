72585b9100d5f2a544c4050f39e273b287c626ff
Haoming Chen <chenhaomingsz@gmail.com>
2017-03-05 17:23:55 -0500

    inverse compositional Algo

--- HW3-Lucas-Kanade Tracking & Background Subtraction/code/testCarSequence.m
+++ HW3-Lucas-Kanade Tracking & Background Subtraction/code/testCarSequence.m
@@ -1,20 +1,14 @@
-
 % load frames
-
+load('../data/carseq.mat')
+frame1 = frames(:,:,1);
+frame2 = frames(:,:,2);
+frame100 = frames(:,:,100);
+frame200 = frames(:,:,200);
+frame300 = frames(:,:,300);
+frame400 = frames(:,:,400);
 
 % implementation
-
-
-
-
-
-
-
-
-
-
-
-
+rect = [60; 117; 146; 152];
 
 
 
